e9d76e18d7e83a28c8d3155c74fd774fa755c11f
gsakshi256 <36441670+gsakshi256@users.noreply.github.com>
2018-05-18 15:01:46 +0530

    usera direct goto notificatiosn page after login

---
+++ spring-boot-web-socket-user-notifications/src/main/java/netgloo/configs/SimpleAuthenticationSuccessHandler.java
@@ -0,0 +1,40 @@
+package netgloo.configs;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+	private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest arg0, HttpServletResponse arg1, Authentication arg2)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		HttpSession session = arg0.getSession();
+		User authUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String username = authUser.getUsername();
+		System.out.println("username" + username);
+		if (username.matches("UserA")) {
+			redirectStrategy.sendRedirect(arg0, arg1, "/notifications");
+		}
+		else
+		{
+			redirectStrategy.sendRedirect(arg0, arg1, "/");
+		}
+	}
+
+}
